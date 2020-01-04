be77351cdcdf224594440cfc6b4b545fd4c5c29e
Zebrass <dainius.trijonis@gmail.com>
2020-01-04 18:39:11 +0200

    On reselected fixed

--- app/src/main/java/com/example/strawpoll/MainActivity.java
+++ app/src/main/java/com/example/strawpoll/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.strawpoll;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -7,20 +8,28 @@ import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private BottomNavigationView bottomNav;
+    private NavController navController;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTheme(R.style.AppTheme);
         setContentView(R.layout.activity_main);
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        BottomNavigationView bottomNav = findViewById(R.id.bottomNavigationView);
-        NavigationUI.setupWithNavController(bottomNav, navController);
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        bottomNav = findViewById(R.id.bottomNavigationView);
+        bottomNav.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener() {
+            @Override
+            public void onNavigationItemReselected(@NonNull MenuItem menuItem) {
 
+            }
+        });
+        NavigationUI.setupWithNavController(bottomNav, navController);
         Intent homeIntent = new Intent(MainActivity.this, SplashScreenActivity.class);
         startActivity(homeIntent);
     }
